4d0e401fe3ddaf9877bda73c8fbf45b04b6fed8b
Weinan Li <weli@redhat.com>
2016-12-22 18:25:53 +0800

    toc

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 fold -w 80 -s Book.md > wrapped.md
-pandoc wrapped.md -t latex -o ThoughtsOnJBossWebServer.pdf
+pandoc --toc --chapters wrapped.md -t latex -o ThoughtsOnJBossWebServer.pdf

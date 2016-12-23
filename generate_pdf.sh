355d41e61b1942210d3080754898e709e46e36cf
Weinan Li <l.weinan@gmail.com>
2016-12-24 01:20:03 +0800

    .

--- generate_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-fold -sw 80 Book.md > wrapped.md
-pandoc --toc --top-level-division=chapter wrapped.md -t latex > ThoughtsOnJBossWebServer.latex
-pandoc --toc --top-level-division=chapter wrapped.md -t latex -o ThoughtsOnJBossWebServer.pdf

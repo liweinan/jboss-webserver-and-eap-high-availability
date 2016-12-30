a7da50a211247d8966efff49c8abf6c63044598b
Weinan Li <l.weinan@gmail.com>
2016-12-31 03:19:05 +0800

    Keep original plan

--- generate_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-fold -sw 80 ThoughtsOnWebServer.md > wrapped.md
-pandoc --toc --top-level-division=chapter --latex-engine=xelatex --template=template.tex wrapped.md -t latex > ThoughtsOnWebServer.latex
-pandoc --toc --top-level-division=chapter --latex-engine=xelatex --template=template.tex wrapped.md -t latex -o ThoughtsOnWebServer.pdf
